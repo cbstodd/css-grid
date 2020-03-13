286f229dd305bb84b99f8c822e24fb5063c8a403
Colper <cbstodd@gmail.com>
2020-03-13 19:21:00 -0400

    Added Greeter message in console for fun

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var greeter = {
+  Welcome: 'You are Running the css-grid app',
+  repo: 'https://github.com/cbstodd/css-grid/blob/master/src/index.js',
+  author: 'Colin Stodd <https://colinstodd.com> | @cbstodd'
+};
+
+console.table(greeter);
